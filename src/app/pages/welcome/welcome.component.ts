fe6a21f088dbaa1d41c523d1bf4e51fee48922f7
888kal <P.Kalyani2@newcastle.ac.uk>
2025-05-16 17:23:00 +0100

    Capture the input in the Welcome Page

--- src/app/pages/welcome/welcome.component.ts
+++ src/app/pages/welcome/welcome.component.ts
@@ -1,11 +1,24 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
+import { FormsModule } from '@angular/forms';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-welcome',
-  imports: [],
+  standalone: true,
+  imports: [CommonModule, FormsModule],
   templateUrl: './welcome.component.html',
-  styleUrl: './welcome.component.scss'
+  styleUrls: ['./welcome.component.scss']
 })
 export class WelcomeComponent {
+  customInput: string = '';
 
+  constructor(private router: Router) {}
+
+  goToChat(prompt: string) {
+    if (!prompt.trim()) return;
+    this.router.navigate(['/chat'], {
+      queryParams: { message: prompt }
+    });
+  }
 }
